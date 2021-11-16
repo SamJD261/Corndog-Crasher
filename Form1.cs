790c7b6c0f68c2aa70b5ef07e9630a4c3779d8fd
Samuel Matthews <samatthews@yahoo.com.au>
2021-11-16 19:40:31 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace button
+{
+    public partial class Button : Form
+    {
+        public Button()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://corndog.io");
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                System.Diagnostics.Process.Start("http://corndog.io");
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.bouncingdvdlogo.com/");
+        }
+    }
+}
